8d297bb0a0b06a023de02493b6e099c46efdd434
prkhch <shjc4623@gmail.com>
2025-01-31 17:04:21 +0900

    feat : player animation

--- Scroll Runner/Assets/Scripts/Player.cs
+++ Scroll Runner/Assets/Scripts/Player.cs
@@ -8,6 +8,7 @@ public class Player : MonoBehaviour
 
     [Header("레퍼런스")]
     public Rigidbody2D playerRigidbody;
+    public Animator playerAnimator;
 
     private bool isGrounded = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -22,11 +23,16 @@ public class Player : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space) && isGrounded) {
             playerRigidbody.AddForceY(jumpForce, ForceMode2D.Impulse);
             isGrounded = false;
+            playerAnimator.SetInteger("state", 1);
         }
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
         if(collision.gameObject.name == "Platform") {
+
+            // 처음 시작할 때는 랜딩 애니메이션 안함
+            if(!isGrounded)playerAnimator.SetInteger("state", 2);
+
             isGrounded = true;
         }
     }
