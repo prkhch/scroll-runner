3af7a7fb56b1311bffcc54e3a8a1877f138f8b48
prkhch <shjc4623@gmail.com>
2025-02-21 11:20:04 +0900

    chore : use prefabs

--- Scroll Runner/Assets/Scripts/VerticalMover.cs
+++ Scroll Runner/Assets/Scripts/VerticalMover.cs
@@ -6,9 +6,10 @@ public class VerticalMover : MonoBehaviour
     [Header("설정")]
     public float speed;
     public float distance;
+    [Tooltip("1 위로, -1 아래")]
+    public int direction;
 
     private Vector3 startPosition;
-    private int direction = 1;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
