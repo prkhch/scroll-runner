f319b8e990cf84b94cf5abed2a7da9657ee2a9c0
prkhch <shjc4623@gmail.com>
2025-02-22 16:51:16 +0900

    feat : attach player on moving platform

--- Scroll Runner/Assets/Scripts/VerticalMover.cs
+++ Scroll Runner/Assets/Scripts/VerticalMover.cs
@@ -4,9 +4,10 @@ public class VerticalMover : MonoBehaviour
 {
 
     [Header("설정")]
+    public bool isVertical;
     public float speed;
     public float distance;
-    [Tooltip("1 위로, -1 아래")]
+    [Tooltip("1 : 상||우 , -1 : 하||좌")]
     public int direction;
 
     private Vector3 startPosition;
@@ -23,16 +24,43 @@ public class VerticalMover : MonoBehaviour
         MovePlatform();
     }
 
-     void MovePlatform()
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        float newY = transform.position.y + (direction * speed * Time.deltaTime);
-
-        if (Mathf.Abs(newY - startPosition.y) >= distance)
+        if (collision.gameObject.CompareTag("Player"))
         {
-            direction *= -1;
-            startPosition.y = newY;
+            collision.transform.SetParent(transform);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.SetParent(null);
+        }
+    }
+
+    void MovePlatform()
+    {
+        if(isVertical) {
+            float newY = transform.position.y + (direction * speed * Time.deltaTime);
+            if (Mathf.Abs(newY - startPosition.y) >= distance)
+            {
+                direction *= -1;
+                startPosition.y = newY;
+            }
+            transform.position = new Vector3(transform.position.x, newY, transform.position.z);
         }
 
-        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+        if(!isVertical) {
+            float newX = transform.position.x + (direction * speed * Time.deltaTime);
+            if (Mathf.Abs(newX - startPosition.x) >= distance)
+            {
+                direction *= -1;
+                startPosition.x = newX;
+            }
+            transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+        }
+
     }
 }
